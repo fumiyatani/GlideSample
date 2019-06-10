44b6fdf20bb2eb7cddacb56b2f6b0b66df96e47f
tanifumiya <fumiyadeveloper@gmail.com>
2019-06-11 01:40:33 +0900

    GIF画像を読み込めるようにし、ループ回数を1回にした

--- GlideSample/app/src/main/java/jp/androidbook/glidesample/MainActivity.kt
+++ GlideSample/app/src/main/java/jp/androidbook/glidesample/MainActivity.kt
@@ -1,12 +1,47 @@
 package jp.androidbook.glidesample
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gifMovie = R.raw.test
+
+        GlideApp.with(this)
+            .load(gifMovie)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    e?.printStackTrace()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    val gifResource = resource as GifDrawable
+                    gifResource.setLoopCount(1)
+                    return false
+                }
+            })
+            .into(gifImage)
     }
 }
